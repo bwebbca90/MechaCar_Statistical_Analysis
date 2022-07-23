59c401b3ca66ab65d00148374d2049b7c5feb140
bwebbca90 <100889472+bwebbca90@users.noreply.github.com>
2022-07-23 16:51:47 -0500

    deliverable 2

--- MechaCarChallenge.RScript.r
+++ MechaCarChallenge.RScript.r
@@ -3,3 +3,17 @@
  mecha <- read.csv('resources/MechaCar_mpg.csv')
 
  summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data=mecha))
+
+ #suspension coil summary statistics
+ sus_coils <- read.csv('resources/Suspension_Coil.csv', header=T, stringsAsFactors = F)
+
+ total_summary <- sus_coils %>% summarize(Mean=mean(PSI), 
+                                         Median=median(PSI), 
+                                         Variance=var(PSI), 
+                                         SD=sd(PSI), 
+                                         .groups = 'keep')
+
+lot_summary <- sus_coils %>% 
+  group_by(Manufacturing_Lot) %>% 
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+  
