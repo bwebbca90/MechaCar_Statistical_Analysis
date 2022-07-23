e62fad755ce09f4a831d8f692e9d95fd953a9652
bwebbca90 <100889472+bwebbca90@users.noreply.github.com>
2022-07-23 17:33:31 -0500

    final deliv update

--- MechaCarChallenge.RScript.r
+++ MechaCarChallenge.RScript.r
@@ -16,4 +16,15 @@
 lot_summary <- sus_coils %>% 
   group_by(Manufacturing_Lot) %>% 
   summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
-  
+
+#t.test for suspension coils
+t.test(sus_coils$PSI, mu=1500)
+
+lot1_data <-subset(sus_coils, Manufacturing_Lot=='Lot1'
+t.test(lot1_data$PSI, mu=1500)
+
+lot2_data<-subset(sus_coils, Manufacturing_Lot=='Lot2')
+t.test(lot2_data$PSI, mu=1500)
+
+lot3_data <-subset(sus_coils, Manufacturing_Lot=='Lot3'
+t.test(lot3_data$PSI, mu=1500)
